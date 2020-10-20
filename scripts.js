703f0aba8ae49aa766d8b25a3e3a0e720be70bdf
Furio Alessio <alefurio@hotmail.it>
2020-10-20 18:00:20 +0200

    senza array

--- scripts.js
+++ scripts.js
@@ -3,30 +3,58 @@
 // per i multipli di 5 stampi buzz
 // per i multipli di 3 e 5 stampi fizzBuzz
 
-var numeri = [];
+// var numeri = [];
+// var modulo3 = 0;
+// var modulo5 = 0;
+//
+// for (var i = 1; i <= 100; i++) {
+//     numeri.push(i);
+// }
+//
+// for (var i = 0; i < numeri.length; i++) { // leggo array da 1 a 100
+//
+//     modulo3 = numeri[i] % 3;
+//     modulo5 = numeri[i] % 5;
+//
+//     if (modulo3 == 0 && modulo5 ==0) {
+//         numeri[i] = 'FizzBuzz';
+//     }
+//
+//     else if (modulo5 == 0 ) {
+//        numeri[i] = 'Buzz';
+//     }
+//
+//     else if (modulo3 == 0 ) {
+//        numeri[i] = 'Fizz';
+//     }
+//
+//     console.log(numeri[i]);
+// }
+
+
+//**********senza array***********
+
+
 var modulo3 = 0;
 var modulo5 = 0;
 
-for (var i = 1; i <= 100; i++) {
-    numeri.push(i);
-}
+for (var i = 1; i < 100; i++) { // leggo array da 1 a 100
 
-for (var i = 0; i < numeri.length; i++) { // leggo array da 1 a 100
+    modulo3 = i % 3;
+    modulo5 = i % 5;
 
-    modulo3 = numeri[i] % 3;
-    modulo5 = numeri[i] % 5;
+    if (modulo3 == 0 && modulo5 == 0) {
+        console.log('fizzbuzz');
 
-    if (modulo3 == 0 && modulo5 ==0) {
-        numeri[i] = 'FizzBuzz';
     }
 
     else if (modulo5 == 0 ) {
-       numeri[i] = 'Buzz';
+       console.log('Buzz');
     }
 
     else if (modulo3 == 0 ) {
-       numeri[i] = 'Fizz';
+       console.log('Fizz');
     }
 
-    console.log(numeri[i]);
+    console.log(i);
 }
