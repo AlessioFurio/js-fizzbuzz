8ae9e5df0f97d49735b3a5b25bb0f43f9c8d90a5
Furio Alessio <alefurio@hotmail.it>
2020-10-20 16:12:28 +0200

    stampa di 100 numeri Fizzbuzz

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// stampa numeri da 1 a 100
+// per i multipli di 3 stampi fizz
+// per i multipli di 5 stampi buzz
+// per i multipli di 3 e 5 stampi fizzBuzz
+
+var numeri = [];
+var modulo3 = 0;
+var modulo5 = 0;
+
+for (var i = 1; i <= 100; i++) {
+    numeri.push(i);
+}
+
+for (var i = 0; i < numeri.length; i++) { // leggo array da 1 a 100
+
+    modulo3 = numeri[i] % 3;
+    modulo5 = numeri[i] % 5;
+
+    if (modulo3 == 0 && modulo5 ==0) {
+        numeri[i] = 'FizzBuzz';
+    }
+
+    else if (modulo5 == 0 ) {
+       numeri[i] = 'Buzz';
+    }
+
+    else if (modulo3 == 0 ) {
+       numeri[i] = 'Fizz';
+    }
+
+    console.log(numeri[i]);
+}
